8ae78fb010fc23512e132d7d178ceeafdba14e7c
rajahjac <jacob.rajah@mail.utoronto.ca>
2020-03-05 23:42:52 -0500

    Pushing lab files

--- lab8/checkpasswd.c
+++ lab8/checkpasswd.c
@@ -15,9 +15,9 @@ int main(void) {
   char user_id[MAXLINE];
   char password[MAXLINE];
 
-  /* The user will type in a user name on one line followed by a password 
+  /* The user will type in a user name on one line followed by a password
      on the next.
-     DO NOT add any prompts.  The only output of this program will be one 
+     DO NOT add any prompts.  The only output of this program will be one
 	 of the messages defined above.
    */
 
@@ -29,8 +29,71 @@ int main(void) {
       perror("fgets");
       exit(1);
   }
-  
   // TODO
+  int fd[2];
+  if(pipe(fd) == -1){
+    perror("pipe");
+    exit(1);
+  }
+
+  int f;
+  if((f = fork()) == -1){
+    perror("fork");
+    exit(1);
+  }
+
+  if(f == 0){
+
+    //write the user id
+    if(write(fd[1], &user_id, 10) == -1){
+      perror("write user");
+      exit(1);
+    }
+    //write the password
+    //printf("%s\n", password);
+    if(write(fd[1], &password, 10) == -1){
+      perror("write password");
+      exit(1);
+    }
+    //done writing, how cant close read cause its needed
+    if(close(fd[1]) == -1){
+      perror("close");
+      exit(1);
+    }
+
+    //redirect stdin to come from pipe
+    if(dup2(fd[0],fileno(stdin)) == -1){
+      perror("dup2");
+      exit(1);
+    }
+
+    execl("./validate", "validate");
+    //will only reach if error
+    perror("exec");
+
+  }
+  //parent
+  else{
+    int status;
+    if(wait(&status) == -1){
+      perror("wait");
+      exit(1);
+    }
+    if(WIFEXITED(status)){
+      int response = WEXITSTATUS(status);
+      if(response == 0){
+        printf(SUCCESS);
+      }
+      else if(response == 2){
+        printf(INVALID);
+      }
+      else{
+        printf(NO_USER);
+      }
+    }
+  }
+
+
 
   return 0;
 }
